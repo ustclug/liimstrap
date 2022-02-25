ddfba4667b95e6354395cf9ac1d47038b0be55a5
iBug <git@ibugone.com>
2022-02-25 21:14:23 +0800

    Replace chameleon.py with chameleon.sh

---
+++ bin/chameleon.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+profile=""
+for x in $(cat /proc/cmdline); do
+  case "$x" in
+    profile=*) eval "$x";;
+    *) ;;
+  esac
+done
+
+case "$profile" in
+  "") ;;
+  iat)
+    F="$HOME/.config/midori/config"
+    sed -i 's|\(^homepage=\).*$|homepage=http://pxe.ustc.edu.cn/liims/index_iat.html|g' "$F"
+    sed -i 's|http://opac\.lib\.ustc\.edu\.cn|http://iat.lib.ustc.edu.cn:88|g' "$F"
+    ;;
+  *) printf "Unknown profile %s\n" "$profile" >&2;;
+esac
