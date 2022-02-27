289d932a5e96837868e5a78caaa303ce70a001c6
iBug <git@ibugone.com>
2022-02-28 00:13:00 +0800

    Replace cron with systemd.timer

--- bin/reset.sh
+++ bin/reset.sh
@@ -2,28 +2,26 @@
 
 X_DISPLAY=":0"
 X_AUTHFILE="/home/$LIIMSUSER/.Xauthority"
-SLEEPTIME=$((RANDOM%600))
 
-echo "Sleep for $SLEEPTIME seconds..."
-sleep $SLEEPTIME
+: "${LIIMSUSER:=liims}" "${IDLETIMEOUT:=30000}"
 
 while true; do
-    IDLE=$(xidle $X_DISPLAY $X_AUTHFILE || echo 32000)
-    echo "> X idle time: $IDLE"
-    test $IDLE -ge 30000 && break
-    sleep 5
+  IDLE=$(/liims/bin/xidle "$X_DISPLAY" "$X_AUTHFILE" || echo 32000)
+  echo "> X idle time: $IDLE"
+  test "$IDLE" -ge "$IDLETIMEOUT" && break
+  sleep 5
 done
 
 echo "Now restart user slim..."
-XDG_RUNTIME_DIR=/run/user/$(id -u $LIIMSUSER) \
-    su $LIIMSUSER -c 'systemctl --user exit'
-systemctl stop slim
+XDG_RUNTIME_DIR="/run/user/$(id -u "$LIIMSUSER")" \
+  su "$LIIMSUSER" -c 'systemctl --user exit'
+systemctl stop slim.service
 sleep 2
 
 echo "Reset /home..."
-su $LIIMSUSER -c "rsync -a --delete /ro/home/$LIIMSUSER/ /home/$LIIMSUSER/"
+su "$LIIMSUSER" -c "rsync -a --delete /ro/home/$LIIMSUSER/ /home/$LIIMSUSER/"
 sleep 2
 
 systemctl daemon-reload
-systemctl start slim
+systemctl start slim.service
 echo "Done!"
