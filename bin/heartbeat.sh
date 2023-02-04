8ca47cb8fd60572a192165cb164c67ffc1881b1a
iBug <git@ibugone.com>
2023-02-05 03:49:34 +0800

    Write image version to /etc/liims_version

--- bin/heartbeat.sh
+++ bin/heartbeat.sh
@@ -3,7 +3,9 @@
 UPTIME="$(cut -d. -f1 /proc/uptime)"
 NETDEV="$(ip -j route get 202.38.93.94 | jq -r .[].dev)"
 test -n "$NETDEV" && MACADDR="$(tr -d : < "/sys/class/net/$NETDEV/address")"
-VERSION="$(grep -Po "version=\K\S*" /proc/cmdline)"
+
+test -r /etc/liims_version && VERSION="$(cat /etc/liims_version)"
+test -z "$VERSION" && VERSION="$(grep -Po "version=\K\S*" /proc/cmdline)"
 test -z "$VERSION" && VERSION="devel"
 
 exec curl -s http://pxe.ustc.edu.cn:3000/ -X POST \
